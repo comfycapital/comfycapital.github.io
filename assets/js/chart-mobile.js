8901c5458fd41af0f888bb8a7b2a1f74ce36dde6
Comfy Capital <99564806+comfycapital@users.noreply.github.com>
2022-04-11 16:20:28 +0200

    Create chart-mobile.js

---
+++ assets/js/chart-mobile.js
@@ -0,0 +1,52 @@
+anychart.onDocumentReady(function () {
+    // create data set on our data
+    var chartData = {
+        rows: [
+            ['J', 0],
+            ['F', 2.82],
+            [' M ', 7.29],
+            ['A'],
+            ['M'],
+            ['J'],
+            ['J'],
+            ['A'],
+            ['S'],
+            ['O'],
+            ['N'],
+            ['D'],
+        ]
+    };
+
+    // create column chart
+    var chart = anychart.column();
+
+    // set chart data
+    chart.data(chartData);
+
+    // turn on chart animation
+    chart.animation(true);
+
+    // set chart title text settings
+    chart.interactivity().hoverMode('single');
+
+    // set axis settings
+    chart.xAxis().value(0).labels(false);
+    chart.xAxis(1).stroke(null).orientation('bottom');
+    chart.yAxis().labels().format('{%Value}%');
+
+    chart.labels(true);
+    chart
+        .tooltip()
+        .title(false)
+        .separator(false)
+        .format('{%Name} {%Value}{decimalSeparator:\\,}%');
+
+    // tune column paddings
+    // chart.barsPadding(0.1).barGroupsPadding(0.9);
+
+    // set container id for the chart
+    chart.container('container-mobile');
+
+    // initiate chart drawing
+    chart.draw();
+});
